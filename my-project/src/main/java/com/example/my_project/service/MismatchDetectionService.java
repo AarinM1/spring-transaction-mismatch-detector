c43925c16a67ca207f17f6538ad4a2a8e3249abf
Aarin <aarinmhashelkar@gmail.com>
2025-07-17 20:31:14 -0500

    added mismatch or missing logic

--- my-project/src/main/java/com/example/my_project/service/MismatchDetectionService.java
+++ my-project/src/main/java/com/example/my_project/service/MismatchDetectionService.java
@@ -1,5 +1,10 @@
 package com.example.my_project.service;
+
+import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -11,18 +16,42 @@ import com.example.my_project.config.secondaryMongoConfig;
 import com.example.my_project.model.BankTransaction;
 
 @Service
-public class MismatchDetectionService{
+public class MismatchDetectionService {
     @Autowired
     private primaryMongoConfig primaryRepo;
     @Autowired
     private secondaryMongoConfig secondaryRepo;
     @Autowired
     private ServiceNowClient serviceNowClient;
+
     @Async
     @Scheduled(cron = "0 0 0 * * MON") // run every Monday at midnight
-    
-    public void compareDatabases(){
+
+    public void compareDatabases() {
         List<BankTransaction> primaryTransactions = primaryRepo.findAll();
         List<BankTransaction> secondaryTransactions = secondaryRepo.findAll();
+        Map<String, BankTransaction> primaryMap = primaryTransactions.stream()
+                .collect(Collectors.toMap(BankTransaction::getTransactionId, Function.identity()));
+        Map<String, BankTransaction> secondaryMap = secondaryTransactions.stream()
+                .collect(Collectors.toMap(BankTransaction::getTransactionId, Function.identity()));
+
+        Set<String> allIds = new HashSet<>();
+        allIds.addAll(primaryMap.keySet());
+        allIds.addAll(secondaryMap.keySet());
+        for (String id : allIds) {
+            BankTransaction tx1 = primaryMap.get(id);
+            BankTransaction tx2 = secondaryMap.get(id);
+
+            if (tx1 == null || tx2 == null) {
+                serviceNowClient.createIncident(id, "Missing transaction in one of the databases",
+                        Instant.now());
+            }
+
+            else if (!tx1.equals(tx2)) {
+                serviceNowClient.createIncident(id, "Field mismatch",
+                        Instant.now());
+            }
+
+        }
     }
 }
